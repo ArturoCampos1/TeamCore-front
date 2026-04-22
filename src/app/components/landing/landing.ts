b4b33d421080befd36c2c4cdd59e7f4c638ef674
Arturo Campos <arturocamposrufo13@gmail.com>
2026-04-22 13:45:19 +0200

    002 - Scroll Reveal dinamico

--- src/app/components/landing/landing.ts
+++ src/app/components/landing/landing.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
 export class Landing {
 
+  ngOnInit() {
+
+    const sr = ScrollReveal({
+      reset: false, // cambia a true si quieres repetir animaciones
+      viewFactor: 0.2
+    });
+
+    // HEADER
+    sr.reveal('.sr-fade-down', {
+      origin: 'top',
+      distance: '20px',
+      duration: 600
+    });
+
+    // HERO (content-1)
+    sr.reveal('.sr-left', {
+      origin: 'left',
+      distance: '60px',
+      duration: 900
+    });
+
+    sr.reveal('.sr-right', {
+      origin: 'right',
+      distance: '60px',
+      duration: 900,
+      delay: 200
+    });
+
+    sr.reveal('.sr-fade', {
+      opacity: 0,
+      duration: 700,
+      delay: 200
+    });
+
+    sr.reveal('.sr-card', {
+      origin: 'bottom',
+      distance: '40px',
+      duration: 800,
+      interval: 150
+    });
+
+    sr.reveal('.sr-zoom', {
+      scale: 0.85,
+      duration: 700,
+      easing: 'ease-in-out'
+    });
+
+  }
 }
