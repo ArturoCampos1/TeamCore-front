e5e155df8761ccec751167013b2a539c5aac61f8
Arturo Campos <arturocamposrufo13@gmail.com>
2026-05-12 00:39:45 +0200

    023 - avance admin dashboard

--- src/app/components/dashboard/gestiones/gestion-empresa/gestion-empresa.ts
+++ src/app/components/dashboard/gestiones/gestion-empresa/gestion-empresa.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../services/auth-service';
+import { Empresa, JwtResponse } from '../../../../models/models';
+import { EmpresaService } from '../../../../services/empresa-service';
 
 @Component({
   selector: 'app-gestion-empresa',
@@ -7,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './gestion-empresa.css',
 })
 export class GestionEmpresa {
+
+    constructor(
+    private authService: AuthService,
+    private empresaService: EmpresaService) {}
+
+    usuario: JwtResponse | null = null;
+    empresa: Empresa | null = null;
+    
+    ngOnInit() {
+      this.usuario = this.authService.getUsuarioLogado();
+      if (!this.usuario) return;
+
+      this.empresaService.findById(this.usuario.idEmpresa).subscribe({
+        next: (data) => {
+          this.empresa = data;
+          console.log("Empresa: " + JSON.stringify(this.empresa))
+        },
+        error: (err) => {
+          console.log("Error: " + err)
+        }
+      })
+    }
 
 }
